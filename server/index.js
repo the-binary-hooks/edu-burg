eeddf6da5d8a0d52bdee9b3c13d708533a16de13
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2021-07-18 19:12:09 +0600

    Adding heroku post script for production build and adding procfile

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+}
 
 const PORT = process.env.port || 5000
 
