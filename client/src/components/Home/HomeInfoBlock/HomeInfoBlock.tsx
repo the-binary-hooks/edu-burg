69e952456047f811482878f0a183d8262f047954
MdNakibulHosenNahid <56796558+MdNakibulHosenNahid@users.noreply.github.com>
2021-07-27 14:02:09 +0600

    style correction in hominfoblock

--- client/src/components/Home/HomeInfoBlock/HomeInfoBlock.tsx
+++ client/src/components/Home/HomeInfoBlock/HomeInfoBlock.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './HomeInfoBlock.css'
 const HomeInfoBlock = () => {
     return (
-        <div className="home-info-block text-white px-5 py-4">
+        <div className="home-info-block text-white px-5 py-4 mt-4">
             <h3>Name</h3>
             <h2>0000</h2>
         </div>
