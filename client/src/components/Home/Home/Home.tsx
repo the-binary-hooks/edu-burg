872deaddf4c514f3dd79805c96e8bcd562339263
Your Name <lamisazamzam@gmail.com>
2021-07-27 20:18:22 +0600

    improved responsiveness in homepage

--- client/src/components/Home/Home/Home.tsx
+++ client/src/components/Home/Home/Home.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import Sidebar from '../../DashboardCommon/Sidebar/Sidebar';
-import HomeInfoBlock from '../HomeInfoBlock/HomeInfoBlock';
-import './Home.css'
+import React from "react";
+import { Container } from "react-bootstrap";
+import Sidebar from "../../DashboardCommon/Sidebar/Sidebar";
+import HomeInfoBlock from "../HomeInfoBlock/HomeInfoBlock";
+import "./Home.css";
 
 const Home = () => {
-
     return (
-        <div className="container-fluid">
+        <Container className="sidebar-content-container">
             <div className="row">
-            <Sidebar />
-            <div className="col-md-10">
-                <h2 className="home-heading text-center">Total</h2>
-                <div className="info-block-container d-flex justify-content-around">
-                <HomeInfoBlock />
-                <HomeInfoBlock />
-                <HomeInfoBlock />
+                <Sidebar />
+                <div className="col-md-10">
+                    <h2 className="home-heading text-center">Total</h2>
+                    <div className="info-block-container d-flex justify-content-around">
+                        <HomeInfoBlock />
+                        <HomeInfoBlock />
+                        <HomeInfoBlock />
+                    </div>
                 </div>
             </div>
-            </div>
-            
-        </div>
+        </Container>
     );
 };
 
