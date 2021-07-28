ede81c4de8fb0a308e4cb10514ffce02cebaff51
Your Name <lamisazamzam@gmail.com>
2021-07-28 11:37:07 +0600

    chatting half done

--- client/src/components/DashboardCommon/Chatting/Chatting.tsx
+++ client/src/components/DashboardCommon/Chatting/Chatting.tsx
@@ -1,4 +1,6 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Col, Container, Form, ListGroup, Row } from "react-bootstrap";
 import Sidebar from "../Sidebar/Sidebar";
 import "./Chatting.css";
 
@@ -8,8 +10,59 @@ const Chatting = () => {
             <Row>
                 <Sidebar />
                 <Col md={10} className="column">
-                    <div className="chat-header">
-                        <h5>Rui Pattricio</h5>
+                    <div style={{ height: "100vh" }}>
+                        <div className="chat-header">
+                            <h5>Rui Pattricio</h5>
+                            <small>Last seen today at 12.00 pm</small>
+                        </div>
+                        <div className="overflow-y-scroll">
+                            <div className="incoming-message">
+                                Hello, how are you?
+                            </div>
+                            <div>
+                                <div className="sent-message">
+                                    Oh! Fine! What about you?
+                                </div>
+                                <div className="sent-message">
+                                    Oh! Fine! What about you?
+                                </div>
+                                <div className="sent-message">
+                                    Oh! Fine! What about you?
+                                </div>
+                                <div className="sent-message">
+                                    Oh! Fine! What about you?
+                                </div>
+                                <div className="sent-message">
+                                    Oh! Fine! What about you?
+                                </div>
+                                <div className="sent-message">
+                                    Oh! Fine! What about you?
+                                </div>
+                                <div className="sent-message">
+                                    Oh! Fine! What about you?
+                                </div>
+                                <div className="sent-message">
+                                    Oh! Fine! What about you?
+                                </div>
+                                <div className="sent-message">
+                                    Oh! Fine! What about you?
+                                </div>
+                            </div>
+                        </div>
+                        <div className="chat-footer">
+                            <Form.Control
+                                type="text"
+                                placeholder="Write Something"
+                                className="message-input"
+                            />
+                            <button className="send-button">
+                                <FontAwesomeIcon
+                                    icon={faPaperPlane}
+                                    size="2x"
+                                    className="send-icon"
+                                />
+                            </button>
+                        </div>
                     </div>
                 </Col>
             </Row>
