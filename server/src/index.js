73686f995fd3c5d55d0b68e169fb62f99de2a1a0
Your Name <lamisazamzam@gmail.com>
2021-07-29 10:14:41 +0600

    mongoose connected

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,39 @@
 import "dotenv";
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
+// import {connectDB} from "./config/db.js";
 import errorHandler from "./middleware/error.js";
 import cors from "cors";
 import authRouter from "./routes/auth.js";
 import privateRouter from "./routes/private.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 dotenv.config();
 
 // Connect DB
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            "mongodb+srv://binaryHook10:LfVQfkn0TGmdZbwx@cluster0.uabc2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+            {
+                auth: { authSource: "admin" },
+                user: "binaryHook10",
+                pass: "LfVQfkn0TGmdZbwx",
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useUnifiedTopology: true,
+                useFindAndModify: true,
+            }
+        );
+    } catch (err) {
+        console.log(err);
+    }
+
+    console.log("MongoDB connected");
+};
+
 connectDB();
 
 app.use(cors());
@@ -30,14 +52,14 @@ app.use("/api/auth", authRouter);
 app.use("/api/private", privateRouter);
 
 // Error Handle
-app.use(errorHandler);
+// app.use(errorHandler);
 
-const server = app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Example app listening`);
 });
 
 // Do not log error
-process.on("unhandledRejection", (err, promise) => {
-    console.log(`logged error: ${err}`);
-    server.close(() => process.exit(1));
-});
+// process.on("unhandledRejection", (err, promise) => {
+//     console.log(`logged error: ${err}`);
+//     server.close(() => process.exit(1));
+// });
