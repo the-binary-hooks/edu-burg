142d115deef550df04a90c4f9185e655a2383278
MdNakibulHosenNahid <56796558+MdNakibulHosenNahid@users.noreply.github.com>
2021-08-03 19:25:49 +0600

    admin dashboard made resposive for mobile device

--- client/src/components/DashboardCommon/Home/HomeInfoBlock/HomeInfoBlock.tsx
+++ client/src/components/DashboardCommon/Home/HomeInfoBlock/HomeInfoBlock.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './HomeInfoBlock.css'
 const HomeInfoBlock = () => {
     return (
-        <div className="home-info-block text-white px-5 py-4 mt-4">
+        <div className="home-info-block text-white text-center px-5 py-4 mt-4" style={{minWidth: '292px'}}>
             <h3>Name</h3>
             <h2>0000</h2>
         </div>
