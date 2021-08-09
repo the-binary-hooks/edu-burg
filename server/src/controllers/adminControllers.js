6a4e13d2ba225254504a13b6cf493d24b43114f8
MdNakibulHosenNahid <56796558+MdNakibulHosenNahid@users.noreply.github.com>
2021-08-09 18:31:29 +0600

    add course functionality added

--- server/src/controllers/adminControllers.js
+++ server/src/controllers/adminControllers.js
@@ -1,4 +1,5 @@
 import Admin from "../models/Admin.js";
+import Course from "../models/Course.js";
 import { sendResponse } from "../utils/sendResponse.js";
 
 export const addAAdmin = async (req, res, next) => {
@@ -35,3 +36,35 @@ export const addAAdmin = async (req, res, next) => {
         }
     });
 };
+
+
+export const addCourse = async(req,res,next) => {
+    const {courseTitle, courseCode, courseTeacher,courseStudents, department} = req.body;
+    const courseData = {
+        courseTitle,
+        courseCode,
+        courseTeacher,
+        courseStudents,
+        department
+    }
+
+    // Create an instance of the Model Course
+    const course = await new Course(courseData);
+    console.log(courseData)
+
+    // Save the course to the course collection
+    course.save((err) => {
+        if (err) {
+           console.log(error);
+        } else {
+            res.status(200).send({success: true,courseData})
+        }
+    });
+
+    // Find the teacher 
+
+    // Update Teacher course array
+
+    // Update student teacher array 
+
+}
--- server/src/routes/adminRoutes.js
+++ server/src/routes/adminRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { addAAdmin } from "../controllers/adminControllers.js";
-
+import {addCourse} from "../controllers/adminControllers.js"
 let router = express.Router();
 
 // Path => http://localhost:5000/api/admin/add
 router.route("/add").post(addAAdmin);
+router.route("/addCourse").post(addCourse)
 
 export default router;
