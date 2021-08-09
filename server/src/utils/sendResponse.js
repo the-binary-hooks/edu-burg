90cbcedcd90621f4a77483f72d4f15aa9f61ccf1
Your Name <lamisazamzam@gmail.com>
2021-08-09 19:00:25 +0600

    merged and pulled

--- server/src/utils/sendResponse.js
+++ server/src/utils/sendResponse.js
@@ -1,4 +1,5 @@
 // Send data to the client
+// Only use for instance of models that has the getSignedToken() method
 export const sendResponse = (addInfo, user, statusCode, res) => {
     const token = user.getSignedToken();
     res.status(statusCode).json({ success: true, token, addInfo });
